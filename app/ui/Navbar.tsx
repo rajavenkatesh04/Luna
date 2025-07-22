4959ba6b0472e8ff2a5afd020e2a5e58c2ef0cd9
rajavenkatesh04 <grv.9604@gmail.com>
2025-07-22 20:11:49 +0530

    Added DarkMode support

--- app/ui/Navbar.tsx
+++ app/ui/Navbar.tsx
@@ -4,9 +4,9 @@ import ToggleSwitch from "@/app/ui/ToggleSwitch";
 
 export default function Navbar() {
     return(
-        <nav className={`max-w-7xl my-5 mx-auto`}>
+        <nav className={`px-4 max-w-7xl my-5 mx-auto`}>
             <div className={`flex justify-between border border-gray-700 rounded-md p-4`}>
-                <h1>Luna.</h1>
+                <h1 className={`font-bold `}>Luna.</h1>
                 <ToggleSwitch />
             </div>
         </nav>
