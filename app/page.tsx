12ed4843d2a287f226f2adbb201d486950a563c0
rajavenkatesh04 <grv.9604@gmail.com>
2025-07-24 12:54:53 +0530

    Modify landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,23 @@
 import {PlayWriteNewZealandFont} from "@/app/ui/fonts";
 import Link from "next/link";
-import {QrCodeIcon, BellAlertIcon, ChatBubbleOvalLeftIcon} from "@heroicons/react/24/outline";
+import {QrCodeIcon, BellAlertIcon, ChatBubbleOvalLeftIcon, UserGroupIcon} from "@heroicons/react/24/outline";
 import Navbar from "@/app/ui/Navbar";
 
 export default function Home() {
     return(
         <>
             <Navbar/>
+            {/*flex flex-col items-center justify-center min-h-screen*/}
 
             <main className={`grow p-4`}>
-                <section className={`flex flex-col items-center justify-center min-h-screen`}>
-                    <h1 className={`${PlayWriteNewZealandFont.className} mb-4 text-xl sm:text-5xl`}>Instant
+                <section className={`text-center space-y-5 mb-5 min-h-screen justify-center content-center`}>
+                    <h3 className={`${PlayWriteNewZealandFont.className} text-5xl md:text-5xl `}>Instant
                         <span className={`bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-emerald-800`}> Event</span>  Updates,
                         <br />
-                    </h1>
-                    <span className={`text-6xl sm:text-9xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400 `}>Simplified.</span>
+                    </h3>
+                    <h1 className={`font-bold text-6xl md:text-9xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400`}>Simplified.</h1>
 
-                    <p className="my-6 max-w-2xl text-sm sm:text-xl text-center mx-auto tracking-wide">
+                    <p className="my-6 max-w-2xl  sm:text-xl text-center mx-auto tracking-wide ">
                         <span className="opacity-70">From college convocations to corporate workshops,</span>{' '}
                         Luna is the <span className={`font-semibold`}>single source of truth</span>{' '}
                         <span className="opacity-90">for your attendees.</span>{' '}
@@ -41,7 +42,7 @@ export default function Home() {
                     <div className={`text-center space-y-5 mb-5`}>
                         <h2 className={`text-5xl md:text-6xl `}><span className={`${PlayWriteNewZealandFont.className} bg-clip-text text-transparent bg-gradient-to-r from-pink-400 to-orange-600`}>Everything</span> You Need.</h2>
                         <h3 className={`text-3xl md:text-4xl`}>Nothing You Don&apos;t.</h3>
-                        <p className={`text-xl mt-5 `}>Focus on your event, not on managing communication chaos. </p>
+                        <p className={`sm:text-xl mt-5 `}>Focus on your event, not on managing communication chaos. </p>
                     </div>
 
                     {/*    Features List */}
@@ -66,6 +67,13 @@ export default function Home() {
                             <h3 className={`text-xl bg-clip-text text-transparent bg-gradient-to-r from-pink-400 to-orange-600`}>Live Chat & Polls</h3>
                             <p>Engage your audience with real-time chat rooms, Q&A sessions, and instant feedback polls.</p>
                         </div>
+
+                    {/*    Feature 4    */}
+                        <div className={`border rounded-md px-10 py-5 flex flex-col items-center text-center justify-center space-y-3`}>
+                            <UserGroupIcon className="h-12 w-12  text-gray-600 dark:text-gray-400" />
+                            <h3 className={`text-xl bg-clip-text text-transparent bg-gradient-to-r from-pink-400 to-orange-600`}>Manage Admins</h3>
+                            <p>Add / Remove admins who can send announcements to your attendees.</p>
+                        </div>
                     </div>
                 </section>
 
@@ -73,7 +81,7 @@ export default function Home() {
             {/*   Call to action */}
                 <section className={`flex flex-col items-center text-center  justify-center min-h-screen`}>
                     <div className={`max-w-5xl mx-auto mb-5 space-y-5`}>
-                        <h2 className={`text-5xl md:text-6xl light:text-black/80`}>Ready to <span className={`bg-clip-text text-transparent bg-gradient-to-r from-teal-300 to-amber-600 ${PlayWriteNewZealandFont.className}`}>Transform</span> Your Events <span className={`bg-clip-text text-transparent bg-gradient-to-r from-teal-300 to-amber-600 `}>?</span></h2>
+                        <h2 className={`text-5xl md:text-6xl light:text-black/80`}>Ready to <span className={`bg-clip-text text-transparent bg-gradient-to-r from-teal-300 to-amber-600 ${PlayWriteNewZealandFont.className} `}>Transform</span> Your Events <span className={`bg-clip-text text-transparent bg-gradient-to-r from-teal-300 to-amber-600 `}>?</span></h2>
                         <p className={`my-6 max-w-2xl text-sm sm:text-xl text-center mx-auto tracking-wide `}>
                             Join thousands of event organizers who&apos;ve discovered the power of seamless,
                             real-time communication. Create your first event for free, no credit card required.
@@ -90,10 +98,11 @@ export default function Home() {
                 </section>
             </main>
 
-            <footer className="py-8 md:py-12 border-t border-slate-200 dark:border-slate-800 bg-slate-50/50 dark:bg-slate-900/50 backdrop-blur-sm">
+            <footer className="py-4 md:py-4 border-t border-t-white bg-gradient-to-r  from-red-100 to-pink-700 backdrop-blur-sm">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 text-center">
-                    <p className=" light:text-slate-500 font-medium">
-                        &copy; 2025 Luna. Crafted with care for event organizers worldwide.
+                    <p className=" light:text-slate-500 text-xs md:text-xl">
+                        &copy; 2025 Luna. <br/>
+                        Crafted with care for event organizers worldwide.
                     </p>
                 </div>
             </footer>
